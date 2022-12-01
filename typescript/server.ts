1831f019ea088bb52095278ac76fff54ad33cb3c
fBergot <florian.bergot564@gmail.com>
2022-12-01 06:17:35 +0100

    début premier fragment twig+ petites choses

--- typescript/server.ts
+++ typescript/server.ts
@@ -5,11 +5,10 @@ import app from "./app";
 
 dotenv.config();
 
-// const utilsConnecter = new utilsConnect();
 const server = http.createServer(app);
 const port = utilsConnect.normalizePort("3000");
 
 server.on("error", (err) => utilsConnect.errorHandler(err, server, port));
-server.on("listening", () => console.log("go"));
+server.on("listening", () => console.log(`# Server ready #`));
 
 server.listen(port, () => console.log(`Ready on port: ${port}`));
